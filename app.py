876dbfd8d0071f9270e4f741ce64cc9e7f45bc1b
js.bae <js.bae@heumlabs.io>
2022-05-12 02:32:38 +0900

    remove useless code

--- app.py
+++ app.py
@@ -12,23 +12,6 @@ from src.todo import add_todo_command_listener
 app = App(token=settings.SLACK_APP_TOKEN, signing_secret=settings.SLACK_SIGNING_SECRET)
 
 
-@app.event("message")
-def message_test(message, logger, *args, **kwargs):
-    ts = message["ts"]
-    text = message.get("text")
-    channel = message["channel"]
-    bot_id = message.get("bot_id")
-    print(ts, text, channel, bot_id)
-    if not bot_id or bot_id != "B033JFZE4Q2":
-        return
-
-    if channel == "C033X1649LH":
-        pass  # github
-
-    if channel == "D02SC7NEATD":
-        pass  # todo
-
-
 add_todo_command_listener(app)
 add_my_command_listener(app)
 
